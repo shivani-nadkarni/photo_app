1fc43481b287e7ed1b3aaed824416e77b484267b
Shivani Nadkarni <shiv.nadk@gmail.com>
2020-09-03 18:15:34 +0530

    Added templates folder. Implemented simple Jinja templating.

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
-@app.route('/index')
-def index():
-	return "Hello World!"
+@app.route('/login')
+def login():
+	return render_template('login.html', title='Login')
