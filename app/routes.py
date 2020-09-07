3873cc1b199d651463c1c90efbf68b57b316fccf
Shivani Nadkarni <shiv.nadk@gmail.com>
2020-09-07 17:45:00 +0530

    Used sessions.

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template, request, g
+from flask import render_template, request, g, session
 import sqlite3
 
 #database path
@@ -22,10 +22,10 @@ def close_connection(exception):
 
 #execute queries
 def query_db(query, args=(), one=False):
-	cur = get_db().execute(query, args)
-	rv = cur.fetchall()
-	cur.close()
-	return (rv[0] if rv else None) if one else rv
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
 
 #initialising schema
 def init_db():
@@ -35,26 +35,62 @@ def init_db():
             db.cursor().executescript(f.read())
         db.commit()
 
+# some other useful functions
+# def convert_image_to_blob(file):
+#   Convert digital data to binary format
+#     with open(filename, 'rb') as file:
+#         blobData = file.read()
+#     return blobData
+
 
 
 #routes
 @app.route('/')
 @app.route('/index')
 def index():
-	return render_template('login.html', title='Login')
+    return render_template('login.html', title='Login')
 
 @app.route('/login', methods=['POST'])
 def login():
-	#fetch user input
-	username = request.form['username']
-	password = request.form['password']
+    #fetch user input
+    username = request.form['username']
+    password = request.form['password']
 
-	#query to fetch user record if exists
-	user = query_db('select * from users where username = ?',
+    #query to fetch user record if exists
+    user = query_db('select * from users where username = ?',
                 [username], one=True)
 
-	if user and password == user['password']:
-		return render_template("my_photos.html", username=username)
-	else:
-		print("login failed")
-		return render_template("login.html", message='Incorrect username/password. Try again!')
+    if user and password == user['password']:
+        session['username'] = username
+        session['user_id'] = user['id']
+        return render_template("my_photos.html", session=session)
+    else:
+        print("login failed")
+        return render_template("login.html", message='Incorrect username/password. Try again!')
+
+"""
+@app.route("/upload", methods=['POST'])
+def upload():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect('my_photos.html')
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect('my_photos.html', session=session, message='Error occurred. Retry uploading')
+        if file:
+            # blob_data = convert_image_to_blob(file)
+            blob_data = file
+            user_id = session.user.id
+            result = query_db('INSERT INTO photos (user_id, photo) VALUES (?, ?)', [user_id, blob_data], one=True)
+            return redirect('my_photos.html', message='file uploaded successfully')
+    return redirect('my_photos.html', session=session)
+"""
+
+@app.route("/logout")
+def logout():
+    return index()
