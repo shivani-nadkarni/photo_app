5a13b0db957f1dff21cce9c0341e65ffd079989c
Shivani Nadkarni <shiv.nadk@gmail.com>
2020-09-13 18:51:34 +0530

    Changed database file path.

--- photo_app/config.py
+++ photo_app/config.py
@@ -2,6 +2,6 @@
 
 class Config(object):
     SECRET_KEY = '1234sasdf//;;'
-    DATABASE = '../photo_app.db'
-    UPLOAD_FOLDER = '../upload_folder'
+    DATABASE = 'photo_app.db'
+    UPLOAD_FOLDER = 'upload_folder'
     ALLOWED_EXTENSIONS = ('png', 'jpg', 'jpeg')
