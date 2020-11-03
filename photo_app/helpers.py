3b3d0888d94f805ec3be66b2925cc63647d64974
Shivani Nadkarni <shiv.nadk@gmail.com>
2020-11-03 10:44:38 +0530

    changes in aws s3 signature

--- photo_app/helpers.py
+++ photo_app/helpers.py
@@ -5,12 +5,14 @@ Specifies upload file method.
 import logging
 import boto3
 from botocore.exceptions import ClientError
+from botocore.client import Config
 from photo_app import app
 
 # Use S3 object storage
 s3_obj = boto3.client('s3',
                       aws_access_key_id=app.config['S3_KEY'],
-                      aws_secret_access_key=app.config['S3_SECRET'])
+                      aws_secret_access_key=app.config['S3_SECRET'],
+                      config=Config(signature_version='s3v4'))
 
 def upload_file_to_s3(s3_file_path, file, bucket_name):
     """This method is used to upload file to the bucket.
